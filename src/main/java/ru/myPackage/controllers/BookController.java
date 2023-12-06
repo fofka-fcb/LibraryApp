ec88336ac1483106d6c96d574967d01df1767336
Stepan6582 <prokofev02@bk.ru>
2023-12-07 02:26:00 +0300

    added show, update, create, delete book added BookMapper

--- src/main/java/ru/myPackage/controllers/BookController.java
+++ src/main/java/ru/myPackage/controllers/BookController.java
@@ -3,9 +3,9 @@ package ru.myPackage.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.myPackage.dao.BookDAO;
+import ru.myPackage.models.Book;
 
 @Controller
 @RequestMapping("/book")
@@ -22,5 +22,40 @@ public class BookController {
     public String bookIndex(Model model) {
         model.addAttribute("books", bookDAO.index());
         return "book/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "book/show";
+    }
+
+    @GetMapping("/new")
+    public String newBook(@ModelAttribute("book") Book book) {
+        return "book/new";
+    }
+
+    @PostMapping
+    public String createBook(@ModelAttribute("book") Book book) {
+        bookDAO.save(book);
+        return "redirect:/book";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editBook(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "book/edit";
+    }
+
+    @PatchMapping("/{id}/update")
+    public String update(@ModelAttribute("book") Book book, @PathVariable("id") int id) {
+        bookDAO.update(book, id);
+        return "redirect:/book";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        bookDAO.delete(id);
+        return "redirect:/book";
     }
 }
--- src/main/java/ru/myPackage/dao/BookDAO.java
+++ src/main/java/ru/myPackage/dao/BookDAO.java
@@ -5,8 +5,10 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.myPackage.models.Book;
+import ru.myPackage.models.People;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
@@ -19,6 +21,32 @@ public class BookDAO {
     }
 
     public List<Book> index() {
-        return jdbcTemplate.query("SELECT * FROM Book", new BeanPropertyRowMapper<>(Book.class));
+        return jdbcTemplate.query("SELECT * FROM Book ORDER BY id", new BookMapper());
+    }
+
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Book WHERE id=?", new Object[]{id}, new BookMapper())
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO Book (people_id, name, author, dob) VALUES (?, ?, ?, ?)",
+                null,
+                book.getName(),
+                book.getAuthor(),
+                book.getDob());
+    }
+
+    public void update(Book updateBook, int id) {
+        jdbcTemplate.update("UPDATE Book SET people_id=?, name=?, author=?, dob=? WHERE id=?",
+                updateBook.getPeopleID(),
+                updateBook.getName(),
+                updateBook.getAuthor(),
+                updateBook.getDob(),
+                id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Book WHERE id=?", id);
     }
 }

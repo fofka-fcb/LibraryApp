78dd9837f8862b4381cfa45bac415d40fab9a2be
Stepan6582 <prokofev02@bk.ru>
2023-12-10 16:24:23 +0300

    added regexp for people (name)

--- src/main/java/ru/myPackage/models/People.java
+++ src/main/java/ru/myPackage/models/People.java
@@ -1,6 +1,7 @@
 package ru.myPackage.models;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,8 @@ public class People {
     int id;
 
     @NotEmpty(message = "Name should not be empty")
+    @Pattern(regexp = "[A-Z]\\w+ [A-Z]\\w+ [A-Z]\\w+",
+            message = "Your full name should be in this format: Surname, Name, Last name")
     String fullName;
 
     @NotEmpty(message = "Date of birthday should not be empty")
